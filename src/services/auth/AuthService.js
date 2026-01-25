4c68f2e6aaa4e5f822da2c09cbb55dd94626b9f1
ralph95 <ralphbaylon95@gmail.com>
2026-01-25 17:34:00 +0800

    OAuth: Updated password default values for oauth

--- src/services/auth/AuthService.js
+++ src/services/auth/AuthService.js
@@ -57,6 +57,8 @@ export const AuthService = {
     const newUser = await UserRepository.createUser({
       email: normalizedEmail,
       name,
+      password: "",
+      passwordConfirm: "",
       verified: true,
       authProvider: "google",
       role_id: role.id,
